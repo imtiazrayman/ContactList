e15632b23ede714596b17ef28f42b630a260637a
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-12 05:14:55 -0400

    Working Modification of Names from specific Number entered

--- app/src/main/java/com/example/contactlist/modifyContact.kt
+++ app/src/main/java/com/example/contactlist/modifyContact.kt
@@ -1,8 +1,11 @@
 package com.example.contactlist
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.CalendarView
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_calender.*
@@ -12,12 +15,42 @@ class modifyContact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calender)
-        if(switch1.isChecked){
-            findViewById<ProgressBar>(R.id.calendarView2).visibility = View.VISIBLE
-        }
-        else{
 
+    }
+    inner class myAsync: AsyncTask<Contact, Unit, Unit>() {
+        override fun doInBackground(vararg p0: Contact) {
+            val myDb= DataBaseManager(applicationContext)
+            myDb.writableDatabase
+            myDb.modify(p0[0])
+            return
         }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            finish()
+        }
+    }
+
+    fun modifyInfo(view: View) {
+        if(TextUtils.isEmpty(phoneEntrytoModify.text))
+        {
+            phoneEntrytoModify.setHint("MUST PUT IN VALUE!")
+            return
+        }
+        if(TextUtils.isEmpty(nameEntrytoModify.text))
+        {
+            nameEntrytoModify.setHint("MUST PUT IN VALUE")
+            return
+        }
+
+        myAsync().execute(Contact(phoneEntrytoModify.text.toString().toInt(), nameEntrytoModify.text.toString(), null))
+
+    }
+
+
+
+    fun cancelModify(view: View) {
+        finish()
     }
 
 }

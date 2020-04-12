503eebc6754cbf21094049b9091d629d3a2f26c5
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-12 00:08:36 -0400

    Created ContactList activity to add new contacts name, phone number and Birthday

--- app/src/main/java/com/example/contactlist/addContact.kt
+++ app/src/main/java/com/example/contactlist/addContact.kt
@@ -2,11 +2,20 @@ package com.example.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_add_contact.*
+import kotlin.time.days
 
 class addContact : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
+
+        calendarView.setOnDateChangeListener {calendarView, y, m, d ->
+            var months = m + 1
+            textView2.text = "Birthday : " + months.toString() +"/" +  d.toString() +"/" + y.toString() }
+
+
     }
 }

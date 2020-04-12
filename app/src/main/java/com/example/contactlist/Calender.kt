79c9c82c5d994eb98caac5cdf6f828c0cc244d0b
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-11 23:08:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/contactlist/Calender.kt
@@ -0,0 +1,12 @@
+package com.example.contactlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calender : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calender)
+    }
+}

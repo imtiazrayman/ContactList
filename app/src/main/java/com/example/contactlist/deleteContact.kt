44693289970eac6d7820a4f2f65f1fca82160668
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-12 05:14:17 -0400

    Working deletion from specific Number entered

--- app/src/main/java/com/example/contactlist/deleteContact.kt
+++ app/src/main/java/com/example/contactlist/deleteContact.kt
@@ -1,7 +1,11 @@
 package com.example.contactlist
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import kotlinx.android.synthetic.main.activity_delete_contact.*
 
 class deleteContact : AppCompatActivity() {
 
@@ -9,4 +13,34 @@ class deleteContact : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete_contact)
     }
+    inner class myAsync: AsyncTask<Contact, Unit, Unit>() {
+        override fun doInBackground(vararg p0: Contact) {
+            val myDb= DataBaseManager(applicationContext)
+            myDb.writableDatabase
+            myDb.delete(p0[0])
+            return
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            finish()
+        }
+    }
+
+    fun cancelDelete(view: View) {
+        finish()
+    }
+
+    fun deleteContact(view: View) {
+        if(TextUtils.isEmpty(phoneEntrytoDelete.text))
+        {
+            phoneEntrytoDelete.setHint("MUST PUT IN VALUE!")
+            return
+        }
+        myAsync().execute(Contact(phoneEntrytoDelete.text.toString().toInt(), null , null))
+
+
+
+    }
+
 }
